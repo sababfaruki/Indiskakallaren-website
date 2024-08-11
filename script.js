2582141c07a1cd205a56a122542caaaa294de30c
sababfaruki <sadmanfarukibd@gmail.com>
2024-08-12 01:14:25 +0200

    added slideshow

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-document.getElementById('mobile-menu-button').onclick = function() {
-    var menu = document.getElementById('mobile-menu');
-    var iconMenu = document.getElementById('icon-menu');
-    var iconClose = document.getElementById('icon-close');
-    
-    // Toggle the menu visibility
-    if (menu.classList.contains('menu-closed')) {
+// JavaScript for toggling the mobile menu
+document.getElementById('mobile-menu-button').onclick = function () {
+  var menu = document.getElementById('mobile-menu');
+  var iconMenu = document.getElementById('icon-menu');
+  var iconClose = document.getElementById('icon-close');
+
+  // Toggle the menu visibility
+  if (menu.classList.contains('menu-closed')) {
+      menu.classList.remove('hidden');
       menu.classList.remove('menu-closed');
       menu.classList.add('menu-open');
       iconMenu.classList.add('hidden');
       iconClose.classList.remove('hidden');
-    } else {
+  } else {
+      menu.classList.add('hidden');
       menu.classList.remove('menu-open');
       menu.classList.add('menu-closed');
       iconMenu.classList.remove('hidden');
       iconClose.classList.add('hidden');
-    }
-  };
-  
+  }
+};
+
+// Slideshow JavaScript
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Show specific slide
+function showSlides(n) {
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+      slideIndex = 1;
+  }
+  if (n < 1) {
+      slideIndex = slides.length;
+  }
+  for (var i = 0; i < slides.length; i++) {
+      slides[i].classList.add("hidden");
+  }
+  slides[slideIndex - 1].classList.remove("hidden");
+}
+
+// Automatic Slideshow
+function autoSlides() {
+  plusSlides(1);
+  setTimeout(autoSlides, 3000); // Change image every 3 seconds
+}
+
+// Start the automatic slideshow
+autoSlides();
